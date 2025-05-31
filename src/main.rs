a91024d7a65fe04de8083c14fb6f72ff7d242f8a
Dorian Bucur <sirdorius@gmail.com>
2025-06-01 01:28:27 +0200

    Added simple chain reaction when hitting with projectile

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,23 @@ use physics::*;
 const WIDTH: f32 = 300.;
 const HEIGHT: f32 = 300.;
 
-#[derive(Component)]
-struct Trader;
+#[derive(Default, PartialEq)]
+enum TraderStatus {
+    #[default]
+    Neutral,
+    Bullish,
+    Bearish,
+}
+
+#[derive(Component, Default)]
+struct Trader {
+    status: TraderStatus,
+}
+
+#[derive(Event)]
+struct TraderChange {
+    entity: Entity,
+}
 
 #[derive(Component)]
 enum Rumor {
@@ -42,8 +57,10 @@ fn main() {
             move_entities,
             check_collisions,
             handle_collisions,
+            update_trader_status,
         ).chain())
         .add_event::<CollisionEvent>()
+        .add_event::<TraderChange>()
         .run();
 }
 
@@ -61,7 +78,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                 translation: Vec3::new(rng.random_range(-WIDTH.. WIDTH), rng.random_range(-HEIGHT.. HEIGHT), 0.),
                 ..Default::default()
             },
-            Trader,
+            Trader::default(),
             Collider {radius: 25.},
             PhysicsBody {
                 velocity: Vec2::new(rng.random_range(-3.0.. 3.0), rng.random_range(-3.0.. 3.0)),
@@ -118,16 +135,27 @@ fn move_entities(
 
 fn handle_collisions(
     mut collisions: EventReader<CollisionEvent>,
+    mut trader_changes: EventWriter<TraderChange>,
     rumor: Query<&Rumor>,
-    trader: Query<&Trader>,
+    mut trader: Query<&mut Trader>,
+    transform: Query<&Transform>,
     mut cmds: Commands,
+    asset_server: Res<AssetServer>,
 ) {
     for collision in collisions.read() {
         if rumor.get(collision.entity1).is_ok() && trader.get(collision.entity2).is_ok() {
-            cmds.entity(collision.entity1).despawn();
+            // TODO good way to not repeat code?
+            println!("TODO handle")
         }
         if rumor.get(collision.entity2).is_ok() && trader.get(collision.entity1).is_ok() {
+            let mut trader = trader.get_mut(collision.entity1).unwrap();
+            if trader.status == TraderStatus::Bullish {
+                continue;
+            }
             cmds.entity(collision.entity2).despawn();
+            trader.status = TraderStatus::Bullish;
+            trader_changes.write(TraderChange {entity: collision.entity1});
+            spawn_taco(&mut cmds, &asset_server, transform.get(collision.entity1).unwrap().translation.xy(), Vec2::new(5., 0.));
         }
     }
 }
@@ -139,23 +167,48 @@ fn player_shooting(
 ) {
     const START_POS: Vec2 = Vec2::new(0., -HEIGHT);
     if key_input.just_pressed(KeyCode::Space) {
-        commands.spawn((
-            Sprite {
-                image: asset_server.load("taco-svgrepo-com.png"),
-                custom_size: Some(vec2(50., 50.)),
-                ..Default::default()
-            },
-            Rumor::Taco,
-            Transform {
-                translation: (START_POS, 0.).into(),
-                ..Default::default()
-            },
-            Collider {radius: 25.},
-            PhysicsBody {
-                velocity: Vec2::new(0., 5.),
-                ..Default::default()
-            },
-            EdgeBehavior::Destroy,
-        ));
+        spawn_taco(&mut commands, &asset_server, START_POS, Vec2::new(0., 5.));
     }
+}
+
+fn update_trader_status(
+    mut traders: Query<(&mut Sprite, &Trader)>,
+    mut events: EventReader<TraderChange>,
+    asset_server: Res<AssetServer>,
+) {
+    for event in events.read() {
+        let (mut sprite, trader) = traders.get_mut(event.entity).unwrap();
+        sprite.image = match trader.status {
+            TraderStatus::Neutral => asset_server.load("ducky.png"),
+            TraderStatus::Bearish => asset_server.load("bear-svgrepo-com.png"),
+            TraderStatus::Bullish => asset_server.load("free-bull-svgrepo-com.png"),
+        };
+    }
+}
+
+// replace with an actual system?
+fn spawn_taco(
+    commands: &mut Commands,
+    asset_server: &Res<AssetServer>,
+    position: Vec2,
+    velocity: Vec2
+) {
+    commands.spawn((
+        Sprite {
+            image: asset_server.load("taco-svgrepo-com.png"),
+            custom_size: Some(vec2(50., 50.)),
+            ..Default::default()
+        },
+        Rumor::Taco,
+        Transform {
+            translation: (position, 0.).into(),
+            ..Default::default()
+        },
+        Collider {radius: 25.},
+        PhysicsBody {
+            velocity,
+            ..Default::default()
+        },
+        EdgeBehavior::Destroy,
+    ));
 }
