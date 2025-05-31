fb1c41b0f81efe6c8611f3b506aafa9dcc59cccc
Dorian Bucur <sirdorius@gmail.com>
2025-05-31 23:31:11 +0200

    Added moving targets and ability to shoot tacos

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 use bevy::asset::AssetMetaCheck;
 use bevy::prelude::*;
+use rand::prelude::*;
+
+pub mod physics;
+
+use physics::*;
+
+const WIDTH: f32 = 300.;
+const HEIGHT: f32 = 300.;
+
+#[derive(Component)]
+struct Trader;
+
+#[derive(Component)]
+enum Rumor {
+    Tariff,
+    Taco,
+}
+
+#[derive(Component)]
+enum EdgeBehavior {
+    Wraparound,
+    Destroy,
+}
+
+#[derive(Component)]
+struct RandomMovement;
 
 fn main() {
     App::new()
@@ -11,13 +37,125 @@ fn main() {
             ..default()
         }))
         .add_systems(Startup, setup)
+        .add_systems(FixedUpdate, (
+            player_shooting,
+            move_entities,
+            check_collisions,
+            handle_collisions,
+        ).chain())
+        .add_event::<CollisionEvent>()
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2d);
-    commands.spawn(Sprite {
-        image: asset_server.load("ducky.png"),
-        ..Default::default()
-    });
+    let mut rng = rand::rng();
+    for _ in 0..10 {
+        commands.spawn((
+            Sprite {
+                image: asset_server.load("ducky.png"),
+                custom_size: Some(vec2(50., 50.)),
+                ..Default::default()
+            },
+            Transform {
+                translation: Vec3::new(rng.random_range(-WIDTH.. WIDTH), rng.random_range(-HEIGHT.. HEIGHT), 0.),
+                ..Default::default()
+            },
+            Trader,
+            Collider {radius: 25.},
+            PhysicsBody {
+                velocity: Vec2::new(rng.random_range(-3.0.. 3.0), rng.random_range(-3.0.. 3.0)),
+                ..Default::default()
+            },
+            RandomMovement,
+            EdgeBehavior::Wraparound,
+        ));
+    }
+}
+
+fn move_entities(
+    mut query: Query<(Entity, &mut PhysicsBody, &mut Transform, Option<&EdgeBehavior>)>,
+    mut cmds: Commands,
+) {
+    for (entity, body, mut transform, maybe_edge) in query.iter_mut() {
+        // transform.translation = ((transform.translation.xy() + body.velocity), 0.).into()
+        transform.translation.x += body.velocity.x;
+        transform.translation.y += body.velocity.y;
+
+        match maybe_edge {
+            Some(EdgeBehavior::Wraparound) => {
+                if transform.translation.x > WIDTH {
+                    transform.translation.x -= WIDTH * 2.
+                }
+                if transform.translation.x < -WIDTH {
+                    transform.translation.x += WIDTH * 2.
+                }
+                if transform.translation.y > HEIGHT {
+                    transform.translation.y -= HEIGHT * 2.
+                }
+                if transform.translation.y < -HEIGHT {
+                    transform.translation.y += HEIGHT * 2.
+                }
+            },
+            Some(EdgeBehavior::Destroy) => {
+                if transform.translation.x > WIDTH {
+                    cmds.entity(entity).despawn();
+                }
+                if transform.translation.x < -WIDTH {
+                    cmds.entity(entity).despawn();
+                }
+                if transform.translation.y > HEIGHT {
+                    cmds.entity(entity).despawn();
+                }
+                if transform.translation.y < -HEIGHT {
+                    cmds.entity(entity).despawn();
+                }
+            },
+            None => (),
+        }
+    }
+}
+
+fn handle_collisions(
+    mut collisions: EventReader<CollisionEvent>,
+    rumor: Query<&Rumor>,
+    trader: Query<&Trader>,
+    mut cmds: Commands,
+) {
+    for collision in collisions.read() {
+        if rumor.get(collision.entity1).is_ok() && trader.get(collision.entity2).is_ok() {
+            cmds.entity(collision.entity1).despawn();
+        }
+        if rumor.get(collision.entity2).is_ok() && trader.get(collision.entity1).is_ok() {
+            cmds.entity(collision.entity2).despawn();
+        }
+    }
+}
+
+fn player_shooting(
+    key_input: Res<ButtonInput<KeyCode>>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    const START_POS: Vec2 = Vec2::new(0., -HEIGHT);
+    if key_input.just_pressed(KeyCode::Space) {
+        commands.spawn((
+            Sprite {
+                image: asset_server.load("taco-svgrepo-com.png"),
+                custom_size: Some(vec2(50., 50.)),
+                ..Default::default()
+            },
+            Rumor::Taco,
+            Transform {
+                translation: (START_POS, 0.).into(),
+                ..Default::default()
+            },
+            Collider {radius: 25.},
+            PhysicsBody {
+                velocity: Vec2::new(0., 5.),
+                ..Default::default()
+            },
+            EdgeBehavior::Destroy,
+        ));
+    }
 }
---
+++ src/physics.rs
@@ -0,0 +1,42 @@
+use bevy::prelude::*;
+
+#[derive(Component, Default)]
+pub struct Collider {
+    pub radius: f32,
+}
+
+/// Does not displace bodies on collision
+// TODO use
+#[derive(Component)]
+#[require(Collider)]
+pub struct AreaTrigger;
+
+#[derive(Component, Default)]
+#[require(Transform)]
+pub struct PhysicsBody {
+    pub velocity: Vec2,
+    pub accel: Vec2,
+}
+
+#[derive(Event)]
+pub struct CollisionEvent {
+	pub entity1: Entity,
+	pub entity2: Entity,
+}
+
+pub fn check_collisions (
+    mut query: Query<(&mut PhysicsBody, &Collider, &mut Transform, Entity, Option<&AreaTrigger>)>,
+    mut collisions: EventWriter<CollisionEvent>,
+) {
+    let mut combinations = query.iter_combinations_mut();
+    while let Some([mut e1, mut e2]) = combinations.fetch_next() {
+        let axis = e1.2.translation - e2.2.translation;
+        if axis.length() < e1.1.radius + e2.1.radius {
+            // handle collision
+            collisions.write(CollisionEvent {
+                entity1: e1.3,
+                entity2: e2.3,
+            });
+        }
+    }
+}
