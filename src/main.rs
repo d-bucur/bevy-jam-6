c1938d72b832e027f287a000b452006583c5fa9d
Dorian Bucur <sirdorius@gmail.com>
2025-06-01 15:32:55 +0200

    Added Tariff projectiles and updated collisions logic

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use physics::*;
 const WIDTH: f32 = 300.;
 const HEIGHT: f32 = 300.;
 
-#[derive(Default, PartialEq)]
+#[derive(Default, PartialEq, Clone, Copy)]
 enum TraderStatus {
     #[default]
     Neutral,
@@ -22,10 +22,18 @@ struct Trader {
     status: TraderStatus,
 }
 
+/// Changes the trader status after some time
 #[derive(Component, Deref, DerefMut)]
+#[require(Trader)]
 struct TraderStatusTimer(Timer);
 
-#[derive(Component)]
+/// Rest time for a trader in which it can't collide with projectiles
+/// Avoids chain reactions that flood the game with projectiles
+#[derive(Component, Deref, DerefMut)]
+#[require(Trader)]
+struct TraderRestTimer(Timer);
+
+#[derive(Component, PartialEq, Clone, Copy)]
 enum Rumor {
     Tariff,
     Taco,
@@ -40,6 +48,22 @@ enum EdgeBehavior {
 #[derive(Component)]
 struct RandomMovement;
 
+#[derive(Component)]
+struct Projectile {
+    owner: Option<Entity>,
+}
+
+#[derive(Resource)]
+struct DonnieShootingLogic {
+    shooting_timer: Timer,
+}
+
+impl Default for DonnieShootingLogic {
+    fn default() -> Self {
+        Self { shooting_timer: Timer::from_seconds(3.0, TimerMode::Repeating) }
+    }
+}
+
 #[derive(Event)]
 struct TraderChange {
     entity: Entity,
@@ -50,6 +74,7 @@ struct SpawnProjectile {
     projectile_type: Rumor,
     position: Vec2,
     direction: Vec2,
+    owner: Option<Entity>, // TODO replace with relationship
 }
 
 fn main() {
@@ -64,7 +89,7 @@ fn main() {
         .add_systems(Startup, setup)
         .add_systems(FixedUpdate, (
             player_shooting,
-            don_shooting,
+            donnie_shooting,
             spawn_projectiles,
             move_entities,
             check_collisions,
@@ -75,6 +100,7 @@ fn main() {
         .add_event::<CollisionEvent>()
         .add_event::<TraderChange>()
         .add_event::<SpawnProjectile>()
+        .init_resource::<DonnieShootingLogic>()
         .run();
 }
 
@@ -148,38 +174,58 @@ fn move_entities(
 }
 
 fn handle_collisions(
+    mut cmds: Commands,
     mut collisions: EventReader<CollisionEvent>,
     mut trader_changes: EventWriter<TraderChange>,
-    rumor: Query<&Rumor>,
-    mut trader: Query<&mut Trader>,
-    transform: Query<&Transform>,
-    mut cmds: Commands,
     mut spawn_events: EventWriter<SpawnProjectile>,
+    mut trader: Query<&mut Trader>,
+    rumor: Query<&Rumor>,
+    trader_query: Query<(&Transform, Option<&TraderRestTimer>)>,
+    projectile: Query<&Projectile>,
 ) {
     for collision in collisions.read() {
-        let entity1_is_trader = trader.get(collision.entity1).is_ok();
-        let entity2_is_trader = trader.get(collision.entity2).is_ok();
+        // TODO cache component gets?
+        let is_rumor_trader = rumor.get(collision.entity1).is_ok() && trader.get(collision.entity2).is_ok();
+        let is_trader_rumor = trader.get(collision.entity1).is_ok() && rumor.get(collision.entity2).is_ok();
 
         let mut check_rumor_vs_trader = |rumor_entity, trader_entity| {
             let mut trader = trader.get_mut(trader_entity).unwrap();
-            if trader.status == TraderStatus::Bullish {
+            let rumor = *rumor.get(rumor_entity).unwrap();
+            if projectile.get(rumor_entity).unwrap().owner == Some(trader_entity)
+                ||(rumor == Rumor::Taco && trader.status == TraderStatus::Bullish) 
+                || (rumor == Rumor::Tariff && trader.status == TraderStatus::Bearish) {
                 return false;
             }
-            cmds.entity(rumor_entity).despawn();
-            // TODO check rumor type
-            trader.status = TraderStatus::Bullish;
+            let (trader_transform, maybe_rest) = trader_query.get(trader_entity).unwrap();
+            if maybe_rest.is_some() {
+                return false;
+            }
+
+            // Change trader status
+            trader.status = match rumor {
+                Rumor::Tariff => TraderStatus::Bearish,
+                Rumor::Taco => TraderStatus::Bullish,
+            };
             trader_changes.write(TraderChange {entity: trader_entity});
-            cmds.entity(trader_entity).insert(TraderStatusTimer(Timer::from_seconds(5., TimerMode::Once)));
-            let position = transform.get(trader_entity).unwrap().translation.xy();
-            spawn_events.write(SpawnProjectile { projectile_type: Rumor::Taco, position, direction: Vec2::new(5., 0.)});
-            spawn_events.write(SpawnProjectile { projectile_type: Rumor::Taco, position, direction: Vec2::new(-5., 0.)});
-            spawn_events.write(SpawnProjectile { projectile_type: Rumor::Taco, position, direction: Vec2::new(0., 5.)});
+            cmds.entity(trader_entity).insert((
+                TraderStatusTimer(Timer::from_seconds(5., TimerMode::Once)),
+                TraderRestTimer(Timer::from_seconds(0.5, TimerMode::Once))
+            ));
+
+            // TODO sometimes loop & crash if lots of stuff is overlapping. Owner not enough, need a disable timer
+            // Spawn chain reaction bullets
+            cmds.entity(rumor_entity).despawn();
+            let position = trader_transform.translation.xy();
+            spawn_events.write(SpawnProjectile { projectile_type: rumor, position, direction: Vec2::new(5., 0.), owner: Some(trader_entity)});
+            spawn_events.write(SpawnProjectile { projectile_type: rumor, position, direction: Vec2::new(-5., 0.), owner: Some(trader_entity)});
+            spawn_events.write(SpawnProjectile { projectile_type: rumor, position, direction: Vec2::new(0., 5.), owner: Some(trader_entity)});
             true
         };
-        if rumor.get(collision.entity1).is_ok() && entity2_is_trader {
+
+        if is_rumor_trader {
             check_rumor_vs_trader(collision.entity1, collision.entity2);
         }
-        if rumor.get(collision.entity2).is_ok() && entity1_is_trader {
+        if is_trader_rumor {
             check_rumor_vs_trader(collision.entity2, collision.entity1);   
         }
     }
@@ -189,15 +235,21 @@ fn player_shooting(
     key_input: Res<ButtonInput<KeyCode>>,
     mut spawn_events: EventWriter<SpawnProjectile>,
 ) {
-    const START_POS: Vec2 = Vec2::new(0., -HEIGHT);
+    const START_POS: Vec2 = Vec2::new(0., -HEIGHT); // change later
     if key_input.just_pressed(KeyCode::Space) {
-        spawn_events.write(SpawnProjectile { projectile_type: Rumor::Taco, position: START_POS, direction: Vec2::new(0., 5.)});
+        spawn_events.write(SpawnProjectile { projectile_type: Rumor::Taco, position: START_POS, direction: Vec2::new(0., 5.), owner: None});
     }
 }
 
-fn don_shooting() {
-    // TODO use timer
-    // normalize bullet types through event
+fn donnie_shooting(
+    mut shooting_logic: ResMut<DonnieShootingLogic>,
+    time: Res<Time>,
+    mut spawn_events: EventWriter<SpawnProjectile>,
+) {
+    const START_POS: Vec2 = Vec2::new(0., HEIGHT); // change later
+    if shooting_logic.shooting_timer.tick(time.delta()).just_finished() {
+        spawn_events.write(SpawnProjectile { projectile_type: Rumor::Tariff, position: START_POS, direction: Vec2::new(0., -5.), owner: None});
+    }
 }
 
 fn update_trader_status(
@@ -217,14 +269,22 @@ fn update_trader_status(
 
 fn tick_trader_timers(
     time: Res<Time>,
-    mut query: Query<(&mut TraderStatusTimer, &mut Trader, Entity)>,
+    mut query_status: Query<(&mut TraderStatusTimer, &mut Trader, Entity)>,
+    query_rest: Query<(&mut TraderRestTimer, Entity)>,
     mut trader_changes: EventWriter<TraderChange>,
+    mut cmds: Commands,
 ) {
-    for (mut timer, mut trader, entity) in &mut query {
+    for (mut timer, mut trader, entity) in &mut query_status {
         if timer.tick(time.delta()).just_finished() {
             trader.status = TraderStatus::Neutral;
             trader_changes.write(TraderChange {entity});
-            // TODO despawn timer
+            cmds.entity(entity).remove::<TraderStatusTimer>();
+        }
+    }
+
+    for (mut timer, entity) in query_rest {
+        if timer.tick(time.delta()).just_finished() {
+            cmds.entity(entity).remove::<TraderRestTimer>();
         }
     }
 }
@@ -236,7 +296,30 @@ fn spawn_projectiles(
 ) {
     for event in spawn_events.read() {
         match event.projectile_type {
-            Rumor::Tariff => todo!(),
+            // TODO refactor
+            Rumor::Tariff => {
+                commands.spawn((
+                    Sprite {
+                        image: asset_server.load("pile-of-poo-svgrepo-com.png"),
+                        custom_size: Some(vec2(50., 50.)),
+                        ..Default::default()
+                    },
+                    Rumor::Tariff,
+                    Transform {
+                        translation: (event.position, 0.).into(),
+                        ..Default::default()
+                    },
+                    Collider {radius: 25.},
+                    PhysicsBody {
+                        velocity: event.direction,
+                        ..Default::default()
+                    },
+                    EdgeBehavior::Destroy,
+                    Projectile {
+                        owner: event.owner
+                    },
+                ));
+            },
             Rumor::Taco => {
                 commands.spawn((
                     Sprite {
@@ -255,6 +338,9 @@ fn spawn_projectiles(
                         ..Default::default()
                     },
                     EdgeBehavior::Destroy,
+                    Projectile {
+                        owner: event.owner
+                    },
                 ));
             },
         }
